abde442b82ab149a364025dea74a4222c5bf16d3
augustomaillo <augusto.maillo@gmail.com>
2023-06-19 23:09:43 -0300

    tests

--- loading_data.py
+++ loading_data.py
@@ -1,6 +1,7 @@
 import soccerdata as sd
 
-fbref = sd.FBref(data_dir="/tmp", no_cache=False, no_store=True, leagues="ENG-Premier League", seasons=2021)
+fbref = sd.FBref(leagues="ENG-Premier League", seasons=2021, no_cache=False)
 
 player_match_stats = fbref.read_player_match_stats(stat_type="passing", match_id='db261cb0')
-player_match_stats.head()
+
+print(player_match_stats.head())
